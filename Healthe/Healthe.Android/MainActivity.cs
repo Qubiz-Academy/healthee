d013d82ed62304fd37c3252a4589e40682f330d8
ileana <ileanapop1811@gmail.com>
2018-06-13 16:04:59 +0300

    changed name to healthee

--- Healthe/Healthe.Android/MainActivity.cs
+++ Healthe/Healthe.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Healthe.Droid
 {
-    [Activity(Label = "Healthe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Healthee", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
